d507bcb0dd0a26e557af81f14251ccaf246b9c5f
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-10-17 21:59:39 +0000

    routes message continuation

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # https://learn.firstdraft.com/lessons/194-helper-methods-part-3
   # https://share.descript.com/view/mQOqBmvJmSo
   # https://grades.firstdraft.com/resources/f220b8c2-f61f-4297-9ee4-becfc532e5c9
+
+  # continue from beginning of partials with inputs
 end
