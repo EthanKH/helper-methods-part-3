fb7bf3723c69509999f893307298ef1a63ebf411
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-10-17 21:02:19 +0000

    added basic bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "movies#index"
   
   resources :movies
+  # https://learn.firstdraft.com/lessons/194-helper-methods-part-3
+  # https://share.descript.com/view/mQOqBmvJmSo
+  # https://grades.firstdraft.com/resources/f220b8c2-f61f-4297-9ee4-becfc532e5c9
 end
