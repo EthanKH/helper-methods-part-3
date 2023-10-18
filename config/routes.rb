7d191b1c87e4da0547640598d30d64523720dfaa
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-10-18 20:38:47 +0000

    index and show looks nice

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # https://learn.firstdraft.com/lessons/194-helper-methods-part-3
   # https://share.descript.com/view/mQOqBmvJmSo
   # https://grades.firstdraft.com/resources/f220b8c2-f61f-4297-9ee4-becfc532e5c9
+  
+  # https://getbootstrap.com/docs/5.3/components/card/
+  # https://getbootstrap.com/docs/5.3/components/card/
+  # https://getbootstrap.com/docs/5.3/layout/grid/
 
   # continue from beginning of partials with inputs
 end
